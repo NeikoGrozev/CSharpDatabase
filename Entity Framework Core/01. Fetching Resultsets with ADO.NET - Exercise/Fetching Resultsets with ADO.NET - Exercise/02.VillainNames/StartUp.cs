5acab9c5243bd60b0af18000cffcd964e8f85f56
Neiko7 <neiko_grozev@abv.bg>
2020-01-19 20:51:45 +0200

    Upload new tasks...

---
+++ Entity Framework Core/01. Fetching Resultsets with ADO.NET - Exercise/Fetching Resultsets with ADO.NET - Exercise/02.VillainNames/StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace VillainNames
+{
+    using System;
+    using System.Data.SqlClient;
+
+    class StartUp
+    {
+        private static string connectionString = "Server=NEIKO\\SQLEXPRESS;" +
+                                                "Database=MinionsDb;" +
+                                                "Integrated Security=true";
+        static void Main()
+        {
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            using (connection)
+            {
+                string selectVillian = @"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                         FROM Villains AS v 
+                                         JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                                         GROUP BY v.Id, v.Name 
+                                         HAVING COUNT(mv.VillainId) > 3 
+                                         ORDER BY COUNT(mv.VillainId)";
+
+                try
+                {
+                    SqlCommand command = new SqlCommand(selectVillian, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    using (reader)
+                    {
+                        while (reader.Read())
+                        {
+                            string name = (string)reader[0];
+                            int count = (int)reader[1];
+
+                            Console.WriteLine($"{name} - {count}");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+        }
+    }
+}
---
+++ Entity Framework Core/01. Fetching Resultsets with ADO.NET - Exercise/Fetching Resultsets with ADO.NET - Exercise/03.MinionNames/StartUp.cs
@@ -0,0 +1,73 @@
+﻿namespace MinionNames
+{
+    using System;
+    using System.Data.SqlClient;
+
+    class StartUp
+    {
+        private static string connectionString = "Server=NEIKO\\SQLEXPRESS;" +
+                                                "Database=MinionsDb;" +
+                                                "Integrated Security=true";
+        static void Main()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                using (connection)
+                {
+                    int idVilian = int.Parse(Console.ReadLine());
+
+                    string selectionVilian = "SELECT Name FROM Villains WHERE Id = @Id";
+                    SqlCommand command = new SqlCommand(selectionVilian, connection);
+                    command.Parameters.AddWithValue("@Id", idVilian);
+                    string vilian = (string)command.ExecuteScalar();
+
+                    if (vilian == null)
+                    {
+                        Console.WriteLine($"No villain with ID {idVilian} exists in the database.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Villain: {vilian}");
+                    }
+                    
+                    string selectMinionName = @$"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) as RowNum,
+                                                m.Name, 
+                                                m.Age
+                                                FROM MinionsVillains AS mv
+                                                JOIN Minions As m ON mv.MinionId = m.Id
+                                                WHERE mv.VillainId = @Id
+                                                ORDER BY m.Name";
+
+                    command = new SqlCommand(selectMinionName, connection);
+                    command.Parameters.AddWithValue("@Id", idVilian);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    using (reader)
+                    {
+                        while (reader.Read())
+                        {
+                            if (reader == null)
+                            {
+                                Console.WriteLine("(no minions)");
+                                break;
+                            }
+
+                            long rowNum = (long)reader[0];
+                            string name = (string)reader[1];
+                            int age = (int)reader[2];
+
+                            Console.WriteLine($"{rowNum}. {name} {age}");
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
