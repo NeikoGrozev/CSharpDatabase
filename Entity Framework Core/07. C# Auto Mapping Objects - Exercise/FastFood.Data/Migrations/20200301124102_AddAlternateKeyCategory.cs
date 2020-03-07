3807fbe1849ca9f678f0a9b2f39b5cf9b2cea20d
Neiko7 <neiko_grozev@abv.bg>
2020-03-07 10:31:15 +0200

    Upload new exercise...

---
+++ Entity Framework Core/07. C# Auto Mapping Objects - Exercise/FastFood.Data/Migrations/20200301124102_AddAlternateKeyCategory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FastFood.Data.Migrations
+{
+    public partial class AddAlternateKeyCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Categories_Name",
+                table: "Categories",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Categories_Name",
+                table: "Categories");
+        }
+    }
+}
